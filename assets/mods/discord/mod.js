54c48b821fe46b92ebf53fdbe44999be810b32c7
ac2pic <ac2pic@gmail.com>
2017-12-24 22:39:53 -0600

    Update mod.js

--- assets/mods/discord/mod.js
+++ assets/mods/discord/mod.js
@@ -7,6 +7,7 @@ var areas = fs.readdirSync('assets/data/areas');
 var area_names = JSON.parse(fs.readFileSync('assets/data/database.json', 'utf8')).areas;
 var _areas = {}; 
 var aName={};
+
 var artList="autumn jungle-city bergen heat-dng jungle heat rhombus-sqr heat-village rookie-harbor".split(" ");
  areas.forEach(function(area) {
     var _maparea = JSON.parse(fs.readFileSync('assets/data/areas/' + area, 'utf8'));
@@ -24,23 +25,59 @@ var artList="autumn jungle-city bergen heat-dng jungle heat rhombus-sqr heat-vil
         });
     });
 });
+function isInMenu() {
+	//this proved to be better at determining whether it is in a menu
+	return Object.keys(ig.vars.get("session.maps")).length === 0;
+}
 function setActivity(){
     if (!rpc)return;
-    var dat={};
-    var state=cc.ig.TextCommand.evaluate("\\v[pvp.active]")=="true"?"PvP":cc.ig.TextCommand.evaluate("\\v[combat.active]")=="true"?"Combat":"Exploration";
-    var area=(simplify.getActiveMapName()||"???").split('.')[0];
-    var elem=["Neutral","Heat","Cold","Wave","Shock"][parseInt(cc.ig.TextCommand.evaluate("\\v[player.element]"))];
-    
+    if(isInMenu()) {
+		rpc.setActivity({
+			state : 'Menu',
+			details : 'In the menu',
+			largeImageKey : "ducklea",
+			largeImageText : "Menu",
+			instance : false
+		});
+		return;
+	} 
+	var isPaused = cc.ig.gameMain.paused;
+	var dat={};
+	var areaName = simplify.getActiveMapName().split(".")[0];
+	var chapterNumber = cc.ig.TextCommand.evaluate("\\v[chapter.current]");
+	var chapterName = cc.ig.TextCommand.evaluate(`\\v[chapter.name.${chapterNumber}]`);
+	var chapter = "";
+	if(chapterNumber === "0") {
+		chapter = 'Prologue - ';
+	} else {
+		chapter = `Chapter ${chapterNumber} - `;
+	}
+	chapter += chapterName;
+    var partySize = parseInt(cc.ig.TextCommand.evaluate("\\v[party.size]"));
+	var partyMax = 3;
+	if(partySize === 1) {
+		partySize = undefined;
+		partyMax = undefined;
+	}
+	var state= "Exploring";
+	if(cc.ig.TextCommand.evaluate("\\v[pvp.active]")=== "true") 
+		state = "In a PvP";
+	else if(cc.ig.TextCommand.evaluate("\\v[combat.active]")=== "true")
+		state = "In combat ";
+	 
+    area= (simplify.getActiveMapName()||"???").split('.')[0];
+    var elementIndex = parseInt(cc.ig.TextCommand.evaluate("\\v[player.element]"))
+	var elem = ["Neutral","Heat","Cold","Shock","Wave"][elementIndex];
     rpc.setActivity({
-        details: _areas[simplify.getActiveMapName()]||"???",
-        state: `${state} (${cc.ig.TextCommand.evaluate("\\v[player.hp]")}/${cc.ig.TextCommand.evaluate("\\v[player.param.hp]")})`,
+        details: `(${chapter})`,
+        state: ((isPaused ? '(Paused) ' : `${state} `) + (_areas[simplify.getActiveMapName()] || areaName )),
         startTimestamp: new Date().getTime()/1000-cc.sc.stats.getStat("player", "playtime"),
-        partySize :parseInt(cc.ig.TextCommand.evaluate("\\v[party.size]")),
-        partyMax:3,
+        partySize: partySize,
+        partyMax: partyMax,
         'smallImageKey':elem.toLowerCase(),
         'smallImageText':elem,
         'largeImageKey':artList.indexOf(area)<0?"ducklea":area.toLowerCase(),
-        'largeImageText':aName[area]||"???",
+        'largeImageText': aName[area]|| "???",
         instance: false,
   });
 }
@@ -49,12 +86,9 @@ function setActivity(){
 
 */
 rpc.on('ready', () => {
-  setActivity();
-
   // activity can only be set every 15 seconds
-  setInterval(() => {
-    setActivity();
-  }, 15e3);
+  setActivity();
+  setInterval(setActivity, 15e3);
 });
 rpc.login("376560840012201984").catch(console.error);
 /*Max hp: cc.ig.TextCommand.evaluate("\\v[player.param.hp]")
